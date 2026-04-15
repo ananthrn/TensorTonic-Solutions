6ff31e0845f7ab5e703d9300ef167bcc60a0699e
Ananth Ravi Narayan <ananth360@gmail.com>
2026-04-14 18:29:38 -0700

    Update solution for cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -6,7 +6,7 @@ def cross_entropy_loss(y_true, y_pred):
     """
     # Write code here
     y_pred = np.array(y_pred)
-    y_true = np.array(y_true)
+    # y_true = np.array(y_true)
     extracted_probs = y_pred[np.arange(len(y_true)), y_true]
     print(extracted_probs)
     return -np.sum(np.log(extracted_probs))/len(y_true)
